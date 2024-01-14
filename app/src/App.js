c6524a1d46860baf14aca5d5703bf8a68ceba25a
Leejaeyeop <dlwoduq78@naver.com>
2024-01-14 19:00:33 +0900

    test code added

--- app/src/App.js
+++ app/src/App.js
@@ -4,64 +4,72 @@ import "./App.css";
 // useDispatch Hook ->  Action을 Dispatch
 import { useSelector, useDispatch } from "react-redux";
 import { decrement, increment, change } from "./store/slice/counterSlice";
-import { Link } from "react-router-dom";
 import useInputs from "./hooks/userInputs";
 import AboutImpl from "./about/about";
+import ProgressBar from "react-divided-progress-bar";
 // import About from ", useCallback./about/about";
-import { memo, useMemo, useCallback } from 'react';
+import { memo, useMemo, useCallback, useState } from "react";
 
-const About = memo(AboutImpl)
-
+const About = memo(AboutImpl);
+const Pm = memo(ProgressBar);
 function App() {
-    const [{ username, email }, onChange, reset] = useInputs({
-        username: "name",
-        email: "email",
-    });
+  const [{ username, email }, onChange, reset] = useInputs({
+    username: "name",
+    email: "email",
+  });
 
-    const count = useSelector((state) => state.counter.value);
-    const dispatch = useDispatch();
+  const count = useSelector((state) => state.counter.value);
 
-    const aboutText = useMemo(() => {
-        return username
-    }, [username]);
+  const [s, ss] = useState(50);
+  const dispatch = useDispatch();
 
-    // const aboutFunciton = useCallback((e) => console.log(e),[])
-    const aboutFunciton = (e) => {console.log(e)}
+  const aboutText = useMemo(() => {
+    return username;
+  }, [username]);
 
+  const aboutFunciton = useCallback((e) => console.log(e), []);
+  // const aboutFunciton = (e) => {console.log(e)}
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img
-                    src={logo}
-                    className="App-logo"
-                    alt="logo"
-                />
-                <main>
-                    <section>
-                        <button onClick={() => dispatch(decrement())}>1씩 감소</button>
-                        <button onClick={() => dispatch(change({value:100}))}>100 변경</button>
-                        <span>{count}</span>
-                        <button onClick={() => dispatch(increment())}>1씩 증가</button>
-                    </section>
-                    {/* <section>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <main>
+          <section>
+            <button onClick={() => dispatch(decrement())}>1씩 감소</button>
+            <button onClick={() => dispatch(change({ value: 100 }))}>
+              100 변경
+            </button>
+            <span>{count}</span>
+            <button onClick={() => dispatch(increment())}>1씩 증가</button>
+          </section>
+          {/* <section>
                         <Link to="/about"> About </Link>
                     </section> */}
-                    <section>
-                        <About text={aboutText} func={aboutFunciton}></About>
-                    </section>
-                    <section>
-                        <input
-                            type="text"
-                            onChange={(e) => onChange(e)}
-                        ></input>
-                        {username}
-                    </section>
-                    <section>{email}</section>
-                </main>
-            </header>
-        </div>
-    );
+          <section>
+            <About text={aboutText} func={aboutFunciton}></About>
+          </section>
+          <section>
+            <input type="text" onChange={(e) => onChange(e)}></input>
+            {username}
+          </section>
+          <section>
+            <button onClick={() => ss((p) => p + 1)}>Pro!</button>
+            <Pm
+              value={s}
+              sections={5}
+              maxValue={100}
+              increaseDuration={1000}
+              color="primary"
+              animated
+              stripped
+              colorChange
+            ></Pm>
+          </section>
+        </main>
+      </header>
+    </div>
+  );
 }
 
 export default App;
